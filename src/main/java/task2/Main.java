24b32fa39a3c725ec321d3c58473715d9870cb13
Ihor <ighor.liu99@gmail.com>
2021-12-07 12:23:42 +0200

    07.12.2021

---
+++ src/main/java/task2/Main.java
@@ -0,0 +1,30 @@
+package task2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+import static task2.Controller.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> dataFromFile = new ArrayList<>();
+        Map<String, Set<Integer>> invertedIndex;
+        if(args[0].equals("--data")) {
+            File file = new File(args[1]);
+
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNext()) {
+                    dataFromFile.add(scanner.nextLine()); // fill the list with data lines from file
+                }
+                scanner.close();
+                invertedIndex = loadInvertedIndex(dataFromFile); // get the Map<String, Set<Integer>>
+                System.out.println(invertedIndex);
+                menu(invertedIndex, dataFromFile); //main logic program with user`s menu
+            } catch (FileNotFoundException e) {
+                System.out.println("Fail");
+            }
+        }
+    }
+}
